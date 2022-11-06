09dd782209c6c35481180c76fbcf4241cf8e7e57
Zikestrike <101480119+Zikestrike@users.noreply.github.com>
2022-11-05 22:18:40 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+enum RadioMessage {
+    message1 = 49434,
+    F = 24246,
+    Msg2 = 42467
+}
+input.onButtonPressed(Button.A, function () {
+    radio.sendMessage(RadioMessage.message1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendMessage(RadioMessage.Msg2)
+})
+radio.onReceivedMessage(RadioMessage.Msg2, function () {
+    basic.showString("BEDTIME")
+})
+radio.onReceivedMessage(RadioMessage.message1, function () {
+    basic.showString("STOP")
+})
+radio.setGroup(1)
